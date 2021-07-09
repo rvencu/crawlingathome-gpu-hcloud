16d7d08918c74e85f322a9946073e585362bfbdd
Richard Vencu <vencu.richard@gmail.com>
2021-07-09 17:17:16 +0000

    try cuda multiprocessing with spawn

--- gpu.py
+++ gpu.py
@@ -5,102 +5,20 @@ import time
 import trio
 import string
 import random
-import pickle
 import shutil
 import curses
 import zipfile
+import clip_filter
 import pandas as pd
 import infrastructure
 from glob import glob
-from copy import copy
 from tqdm import tqdm
 from pathlib import Path
 from colorama import Fore
-sys.path.append('./crawlingathome-worker/')
-from PIL import ImageFile
-from multiprocessing import JoinableQueue, Process, Pool
-from pssh.clients import ParallelSSHClient, SSHClient
 from gevent import joinall
-
-ImageFile.LOAD_TRUNCATED_IMAGES = True  # https://stackoverflow.com/a/47958486
-
-def df_clipfilter(df):
-    sim_threshold = 0.3
-    underaged_text = ["teen", "kid", "child", "baby"]
-    import clip_filter
-
-    clip = clip_filter.CLIP()
-    img_embedding, similarities = clip.preprocess_images(df)
-    tmp_embed = copy(img_embedding)
-    for i, img_embed in enumerate(tmp_embed):
-        if similarities[i] < sim_threshold:
-            df.drop(i, inplace=True)
-            img_embedding.remove(img_embed)
-            continue
-
-        # get most similar categories
-        nsfw_prob = clip.prob(img_embed, clip.categories)
-        df.at[i, "NSFW"] = "UNSURE"
-        df.at[i, "similarity"] = similarities[i]
-        if nsfw_prob[0] < 19 and nsfw_prob[1] < 19:
-            df.at[i, "NSFW"] = "UNLIKELY"
-            continue
-        elif nsfw_prob[0] >= 19 and nsfw_prob[1] >= 19:
-            df.at[i, "NSFW"] = "NSFW"
-
-        underage_prob = clip.prob(img_embed, clip.underaged_categories)
-        if (
-            underage_prob[0] < 4
-            or underage_prob[1] < 4
-            or any(x in df.at[i, "TEXT"] for x in underaged_text)
-        ):
-            df.drop(i, inplace=True)
-            img_embedding.remove(img_embed)
-            continue
-
-        animal_prob = clip.prob(img_embed, clip.animal_categories)
-        if animal_prob[0] > 20:
-            df.drop(i, inplace=True)
-            img_embedding.remove(img_embed)
-
-    df.reset_index(drop=True, inplace=True)
-    return df, img_embedding
-
-
-def df_tfrecords(df, output_fname):
-    import tensorflow as tf
-    from tfr_image.utils import bytes_feature, int64_feature
-
-    def image_to_tfexample(sample_id, image_data, image_format, height, width, caption):
-        return tf.train.Example(
-            features=tf.train.Features(
-                feature={
-                    "sampleID": bytes_feature(sample_id),
-                    "image": bytes_feature(image_data),
-                    "format": bytes_feature(image_format),
-                    "label": bytes_feature(caption),
-                    "height": int64_feature(height),
-                    "width": int64_feature(width),
-                }
-            )
-        )
-
-    with tf.io.TFRecordWriter(output_fname) as tfrecord_writer:
-        for i in range(len(df)):
-            df_image = df.iloc[i]
-            image_fname = df_image["PATH"]
-            file_type = image_fname.split(".")[-1]
-            with tf.io.gfile.GFile(image_fname, "rb") as f:
-                image_data = f.read()
-            example = image_to_tfexample(
-                str(df_image["SAMPLE_ID"]).encode("utf_8"),
-                image_data,
-                file_type.encode("utf_8"),
-                df_image["HEIGHT"],
-                df_image["WIDTH"],
-                df_image["TEXT"].encode("utf_8"),
-            )
-            tfrecord_writer.write(example.SerializeToString())
+sys.path.append('./crawlingathome-worker/')
+from multiprocessing import JoinableQueue, Process
+from pssh.clients import ParallelSSHClient, SSHClient
 
 
 def incoming_worker(workers, queue: JoinableQueue, inpsize: JoinableQueue, errors: JoinableQueue):
@@ -184,7 +102,7 @@ def outgoing_worker(queue: JoinableQueue, errors: JoinableQueue, local=False):
                 # send GPU results
                 shutil.make_archive(base + "/gpujobdone", "zip", base, "save")
 
-                cmd = aclient.scp_send(base + "/gpujobdone.zip", "gpujobdone.zip")
+                aclient.scp_send(base + "/gpujobdone.zip", "gpujobdone.zip")
                 if local:
                     os.system(f"mv {base}/gpujobdone.zip results/{time.time()}.zip")
                     aclient.execute("touch gpulocal")
@@ -195,13 +113,12 @@ def outgoing_worker(queue: JoinableQueue, errors: JoinableQueue, local=False):
                 aclient.disconnect()
                 queue.task_done()
 
-                    #print(f"[{ip}] resuming job with GPU results")
             else:
                 time.sleep(5)
         except Exception as e:
             print(f"some outbound problem occured: {e}")
 
-def gpu_worker(inbound: JoinableQueue, outbound: JoinableQueue, counter: JoinableQueue, errors: JoinableQueue, gpuflag: JoinableQueue):
+def gpu_worker(inbound: JoinableQueue, outbound: JoinableQueue, counter: JoinableQueue, errors: JoinableQueue, gpuflag: JoinableQueue, test=False):
     print (f"gpu worker started")
     while True:
         if inbound.qsize() > 0:
@@ -226,21 +143,8 @@ def gpu_worker(inbound: JoinableQueue, outbound: JoinableQueue, counter: Joinabl
             dlparse_df["PATH"] = "./" + \
                 ip.replace(".", "-") + "/" + dlparse_df["PATH"]
 
-            filtered_df, img_embeddings = df_clipfilter(dlparse_df)
-            filtered_df.to_csv(output_folder + out_fname +
-                            ".csv", index=False, sep="|")
-
-            img_embeds_sampleid = {}
-            for i, img_embed_it in enumerate(img_embeddings):
-                dfid_index = filtered_df.at[i, "SAMPLE_ID"]
-                img_embeds_sampleid[str(dfid_index)] = img_embed_it
-            with open(f"{output_folder}image_embedding_dict-{out_fname}.pkl", "wb") as f:
-                pickle.dump(img_embeds_sampleid, f)
-
-            df_tfrecords(
-                filtered_df,
-                f"{output_folder}crawling_at_home_{out_fname}__00000-of-00001.tfrecord",
-            )
+            final_images = clip_filter.filter(dlparse_df, out_fname, output_folder)
+            errors.put(f"last filtered {final_images} images")
 
             outbound.put(ip)
             inbound.task_done()
@@ -310,7 +214,7 @@ def monitor2(nodes, inbound, outbound, counter, inpsize, stdscr, errors, gpuflag
             stdscr.addstr(2,0,"-----------  ")
         stdscr.addstr(2,13,f"GPU jobs done:    {gpujobsdone}")
 
-        stdscr.addstr(3,0,f"GPU velocity: {round((gpujobsdone/(time.time()-start)), 2)} jobs/s")
+        stdscr.addstr(3,0,f"GPU velocity: {round(60*(gpujobsdone/(time.time()-start)), 2)} jobs/m")
         if errors.qsize() > 0:
             msg = errors.get()
             errors.task_done()
@@ -368,6 +272,7 @@ if __name__ == "__main__":
                 workers.append(line.strip("\n"))
 
     try:
+
         # initial cleanup - delete all working files in case of crash recovery
         reg_compile = re.compile(r"^\d{1,3}-\d{1,3}-\d{1,3}-\d{1,3}$")
         for root, dirnames, filenames in os.walk("."):
@@ -379,7 +284,6 @@ if __name__ == "__main__":
                     shutil.rmtree(dir)
 
         
-
         #initialize 3 joinable queues to transfer messages between multiprocess processes
         inbound = JoinableQueue()
         outbound = JoinableQueue()
@@ -393,16 +297,19 @@ if __name__ == "__main__":
         time.sleep(5)
         otb = Process(target=outgoing_worker, args=[outbound, errors, local], daemon=True).start()
         time.sleep(5)
-        #P = Pool(processes=3)
-        #gpu = P.map(gpu_worker, [inbound, outbound, counter])
-        gpu1 = Process(target=gpu_worker, args=[inbound, outbound, counter, errors, gpuflag], daemon=True).start()
-        gpu2 = Process(target=gpu_worker, args=[inbound, outbound, counter, errors, gpuflag], daemon=True).start()
-        gpu3 = Process(target=gpu_worker, args=[inbound, outbound, counter, errors, gpuflag], daemon=True).start()
+        
+        #gpu_worker(inbound, outbound, counter, errors, gpuflag)
 
-        stdscr = curses.initscr()
+        gpu1 = Process(target=gpu_worker, args=[inbound, outbound, counter, errors, gpuflag]).start()
+        #gpu2 = Process(target=gpu_worker, args=[inbound, outbound, counter, errors, gpuflag]).start()
+        #gpu3 = Process(target=gpu_worker, args=[inbound, outbound, counter, errors, gpuflag]).start()
+
+        #stdscr = curses.initscr()
         #sys.stdout.close()
-        #monitor(nodes, inbound, outbound, counter)
-        curses.wrapper(monitor2(nodes, inbound, outbound, counter, inpsize, stdscr, errors, gpuflag))
+        monitor(nodes, inbound, outbound, counter)
+        #curses.wrapper(monitor2(nodes, inbound, outbound, counter, inpsize, stdscr, errors, gpuflag))
+        while True:
+            time.sleep(5)
 
     except KeyboardInterrupt:
         curses.nocbreak()
