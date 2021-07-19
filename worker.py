1f8e746d532460e513378e10fae679c208a67cee
Richard Vencu <vencu.richard@gmail.com>
2021-07-19 10:22:06 +0000

    fix bloom download

--- worker.py
+++ worker.py
@@ -281,7 +281,7 @@ if __name__ == "__main__":
     # initialize stats variables for previous job
     last = 0
 
-    os.system("rsync -zh archiveteam@88.198.2.17::bloom crawlingathome-gpu-hcloud/blocklists/")
+    os.system("rsync -zh archiveteam@88.198.2.17::bloom/* crawlingathome-gpu-hcloud/blocklists")
 
     # this makes a loop to download new jobs while the script is running
     # normally it reads while client.jobCount() > 0
