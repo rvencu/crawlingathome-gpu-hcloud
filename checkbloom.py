12472051ee83a3b5b2fce1948274e7451787d365
Richard Vencu <vencu.richard@gmail.com>
2021-08-02 20:07:32 +0000

    tidy up

--- checkbloom.py
+++
@@ -1,5 +0,0 @@
-import sys
-from bloom_filter2 import BloomFilter
-bloom = BloomFilter(max_elements=80000000, error_rate=0.01, filename=("blocklists/bloom.bin",-1))
-hash = sys.argv[1]
-print (hash in bloom)
