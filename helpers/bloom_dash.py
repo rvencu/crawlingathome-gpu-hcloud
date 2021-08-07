5f1082cfde78dbb6eb8951dba2f7e743e59660a4
Richard Vencu <vencu.richard@gmail.com>
2021-08-07 19:48:53 +0000

    add json reports for tracker usage

--- helpers/bloom_dash.py
+++ helpers/bloom_dash.py
@@ -9,5 +9,25 @@ async def index(request):
     with open('/home/archiveteam/dashboard.txt', 'rt') as file:
         reply = file.read()
     return 200, {'Content-Type': 'text/html; charset=utf-8'}, "<html><body><style>body{font-family: monospace}</style>" + reply + "</body></html>"
-        
-app.run(host='0.0.0.0', port=80)
+
+@app.get('/stats')
+async def index(request):
+    uniques = []
+    total = []
+    clipped = []
+    with open('/home/archiveteam/dashboard.txt', 'rt') as file:
+        lines = file.readlines()
+        for line in lines:
+            if line.startswith("M unique pairs"):
+                chunks = line.split("<br/>")
+                uniques.append(chunks[0].split(" ")[-1])
+                total.append(chunks[1].split(" ")[-1])
+                clipped.append(chunks[2].split(" ")[-1])
+
+    reply = '{"total": {"uniques":' + uniques[0] + ',"pairs":' + total[0] + ',"clips":' + clipped[0] + '},'
+    reply += '"day": {"uniques":' + uniques[1] + ',"pairs":' + total[1] + ',"clips":' + clipped[1] + '},'
+    reply += '"week": {"uniques":' + uniques[2] + ',"pairs":' + total[2] + ',"clips":' + clipped[2] + "}}"
+
+    return 200, {'Content-Type': 'application.json; charset=utf-8'}, reply
+
+app.run(host='0.0.0.0', port=8080)
--- helpers/capacity.py
+++ helpers/capacity.py
@@ -10,7 +10,9 @@ path = "/home/archiveteam/CAH/gpujobs"
 # about the given path
 @app.get('/disk')
 async def index(request):
-    stat = shutil.disk_usage(path)
-    return 200, {'Content-Type': 'text/html; charset=utf-8'}, "<html><body><style>body{font-family: monospace}</style>" + stat + "</body></html>"
+    stat = str(shutil.disk_usage(path))
+    stat = stat.split("(")[1].split(")")[0]
+    stat = '{"' + stat.replace("=",'":').replace(", ",', "') + "}"
+    return 200, {'Content-Type': 'application/json; charset=utf-8'},  stat
         
-app.run(host='0.0.0.0', port=80)
+app.run(host='0.0.0.0', port=8080)
