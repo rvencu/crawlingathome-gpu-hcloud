0639d5b93165302adb6110ca59df6785eefc5fdb
Richard Vencu <vencu.richard@gmail.com>
2021-08-09 13:58:35 +0000

    enhancements

--- helpers/capacity.py
+++ helpers/capacity.py
@@ -1,5 +1,7 @@
 # use in staging server to return disk capacity level
 import shutil
+import json
+import os
 
 from aioserver import Application
 app = Application()
@@ -12,7 +14,11 @@ path = "/home/archiveteam/CAH/gpujobs"
 async def index(request):
     stat = str(shutil.disk_usage(path))
     stat = stat.split("(")[1].split(")")[0]
-    stat = '{"' + stat.replace("=",'":').replace(", ",', "') + "}"
-    return 200, {'Content-Type': 'application/json; charset=utf-8'},  stat
+    stat = '{"' + stat.replace('=','":').replace(', ',', "') + '}'
+    json_object = json.loads(stat)
+    json_object["utilization"] =  round(json_object["used"]/json_object["total"], 2)
+    json_object["jobscount"] = len([name for name in os.listdir(path) if os.path.isfile(os.path.join(path, name))])
+
+    return 200, {'Content-Type': 'application/json; charset=utf-8'},  json.dumps(json_object)
         
 app.run(host='0.0.0.0', port=8080)
