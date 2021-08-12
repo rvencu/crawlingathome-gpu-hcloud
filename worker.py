880b679b1a2877d262bd8ed56080df459f972ad9
Richard Vencu <vencu.richard@gmail.com>
2021-08-12 06:11:22 +0000

    fix

--- worker.py
+++ worker.py
@@ -418,7 +418,7 @@ if __name__ == "__main__":
             start = time.time()
 
             # convert to dataframe and save to disk (for statistics and generating blocking lists)
-            parsed_df = pd.DataFrame(parsed_data, columns=["URL","TEXT","LICENSE"])
+            parsed_df = pd.DataFrame(parsed_data, columns=["URL","TEXT","LICENSE","DOMAIN"])
             parsed_df = parsed_df.drop_duplicates(subset=["URL"])
             parsed_df.to_csv(output_folder + out_fname + "_parsed.csv", index=False, sep="|")
 
