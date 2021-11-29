ee3e96017e9a586fd139af9a31e8555adec61b5e
Richard Vencu <vencu.richard@gmail.com>
2021-11-29 21:03:17 +0200

    dl multilanguage on hetzner

--- dbdl.py
+++ dbdl.py
@@ -373,6 +373,9 @@ if __name__ == "__main__":
     params = config()
     engine = create_engine(f'postgresql://{params["user"]}:{params["password"]}@{params["host"]}:5432/{params["database"]}', pool_size=procs, max_overflow=int(procs*1.5), pool_pre_ping=True )
 
+    #this log file can grow quite a lot
+    os.system("rm errors.txt")
+
     for i in range(procs):
         Process(target=worker, args=[engine, params, i], daemon=True).start()
 
