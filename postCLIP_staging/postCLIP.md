ca07dbd2e37638285fe95c7b790095499a610cc3
Richard Vencu <vencu.richard@gmail.com>
2022-01-26 08:10:00 +0200

    more scripts

---
+++ postCLIP_staging/postCLIP.md
@@ -0,0 +1,3 @@
+This server will store the results of CLIP inference. Once arrived it arranges the files in date and time based subfolders, and will update the dataset bloom filters to prevent further duplication. 
+
+The bloom log also exposes a dashboard with velocity information (24h, 7d and 1 month)
--- preCLIP_staging/cleanup.py
+++ preCLIP_staging/cleanup.py
@@ -1,53 +1,95 @@
 import os
+import sys
 import time
 import requests
+import argparse
 from glob import glob
 import os.path as path
 from datetime import datetime
 from multiprocessing import Process, Queue
+from sqlalchemy import create_engine
+from configparser import ConfigParser
 
-def worker(i:int, q: Queue):
+def config(filename='database.ini', section='cah_production'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+    return db
+
+def confirm_delete(engine, uuid, jobset="en"):
+    jobtable = "jobs"
+    if jobset != "en":
+        jobtable = f"jobs_{jobset}"
+    select_stmt1 = f"select count(*) from {jobtable} where status > 1 and jobid = '{uuid}'"
+    conn = engine.raw_connection()
+    cur = conn.cursor()
+    cur.execute(select_stmt1)
+    jobcount = int(cur.fetchone()[0])
+    conn.commit()
+    cur.close()
+    conn.close()
+    return jobcount
+
+def worker(engine, q: Queue, jobset = "en"):
+    jobspath = '/mnt/md0/gpujobs/'
+    if jobset == "intl":
+        jobspath = '/mnt/md0/gpujobsml/'
+    if jobset == "nolang":
+        jobspath = '/mnt/md0/gpujobsnolang/'
     while q.qsize()>0:
         try:
-            files_batch = q.get_nowait()
-            start2 = time.time()
-            #print(f"[{i}] 50 batch started")
-            r = requests.post("http://cah.io.community/api/isCompleted", json={"addresses": files_batch})
-            eligible = r.json()
-            to_delete = ["/home/archiveteam/CAH/gpujobs/" + x.split(" ")[1] + ".tar.gz" for x in eligible]
-            #print(f"[{i}] starting delete after {round(time.time()-start2, 2)}")
-            for file in to_delete:
+            uuid = q.get_nowait()
+            if confirm_delete(engine, uuid, jobset)==1:
+                file = f"{jobspath}{uuid}.tar.gz"
                 if os.path.isfile(file) and os.path.getmtime(file) < time.time() - 60*60: # this makes the code more robust
                     os.remove(file)
-            #print(f"[{i}] batch done in {round(time.time()-start2, 2)}")
+                    print(f"deleted {file}")
         except Exception as e:
-            #print (f"[{i}] worker raised error {e}")
+            print (f"worker raised error {e}")
             pass
 
+parser = argparse.ArgumentParser(prog=sys.argv[0], usage='%(prog)s -s/--set')
+parser.add_argument("-s","--set",action='append',help="Choose current set (en, nolang, intl)",required=False)
+args = parser.parse_args()
+
+params = config()
+engine = create_engine(f'postgresql://{params["user"]}:{params["password"]}@{params["host"]}:5432/{params["database"]}',pool_size=25, max_overflow=50)
+
+jobset = "en"
+
+if args.set is not None:
+    jobset = args.set[0]
+
+jobspath = '/mnt/md0/gpujobs/*.tar.gz'
+if jobset == "intl":
+    jobspath = '/mnt/md0/gpujobsml/*.tar.gz'
+if jobset == "nolang":
+    jobspath = '/mnt/md0/gpujobsnolang/*.tar.gz'
+
 now = datetime.now().strftime("%Y/%m/%d_%H:%M")
-list_of_files = glob('/home/archiveteam/CAH/gpujobs/*.tar.gz')
+list_of_files = glob(jobspath)
 frm = len(list_of_files)
 
 start = time.time()
-i = 0
-files_batch = []
 q = Queue()
 procs = []
 for i in range(10):
-    procs.append(Process(target=worker, args=[i, q]))
-
-#print (f"starting cleanup of {frm}")
+    procs.append(Process(target=worker, args=[engine, q, jobset]))
 
 for file in list_of_files:
-    if time.time() - path.getmtime(file) < 100:
+    if time.time() - path.getmtime(file) < 300:
         continue
-    uuid = file.split("/")[5].split(".")[0]
-    files_batch.append(f"rsync {uuid}")
-    i += 1
-    if i%50 == 0:
-        q.put(files_batch)
-        files_batch = []
-q.put(files_batch)
+    uuid = file.split("/")[4].split(".")[0]
+    q.put(uuid)
 
 time.sleep(20)
 
@@ -56,7 +98,7 @@ for proc in procs:
 for proc in procs:
     proc.join()
 
-list_of_files = glob('/home/archiveteam/CAH/gpujobs/*.tar.gz')
+list_of_files = glob(jobspath)
 end = len(list_of_files)
 
 with open("jobs.txt","wt") as f:
